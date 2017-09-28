1b39cd0d0650ad9f5c5ba95d91df383799e48501
webwatson92 <webwatson92@gmail.com>
2017-09-28 23:37:49 +0200

    Application de cartographie

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,6 +8,17 @@
 				 <div id="mapid"></div>
 			</div>
 		</div>
+		<div class="row">
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- 728*90 plat bloc -->
+			<ins class="adsbygoogle"
+			     style="display:inline-block;width:728px;height:90px"
+			     data-ad-client="ca-pub-3624912069544968"
+			     data-ad-slot="4634569884"></ins>
+			<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
        
 @stop
