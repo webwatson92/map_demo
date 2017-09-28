8c05ff380e09435f6db0b8358a3578fca434a0db
webwatson92 <webwatson92@gmail.com>
2017-09-28 23:28:43 +0200

    Application de cartographie

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,57 @@
-@extends('layouts.app')
+@extends('layouts/app', ['title' => 'Connexion']) 
+@include('layout/partials/_nav')
 
 @section('content')
+
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+    <div class="row"> 
+        <div class="col-sm-6">
+                <div class="row"> 
+                     <p style="margin-top: 10px;font-size: 1.5em" class="acceuil">Avec {{config('app.name')}},  restez en contact avec toutes <br>  les universités et grandes écoles de l'afrique.</p>
+                </div>
+                <div class="row">
+                     <img style="width: 30em" src="img/iphone6-3.jpg">
+                </div>  
+        </div>
+        <div class="col-sm-6">
+              <div class="row">
+      <div class="">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Connectez-vous !</h3>
+        </div>
+          <div class="panel-body">
+            <form accept-charset="UTF-8" role="form" action="{{ route('login') }}" method="POST">
+              <fieldset>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
+                <div class="form-group" {{ $errors->has('email') ? 'has-error' : '' }}>
+                  <input class="form-control" required autofocus placeholder="monmail@example.com" name="email" type="text">
+                        @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @endif
                 </div>
-            </div>
+                <div class="form-group" {{ $errors->has('password') ? 'has-error' : '' }}>
+                  <input class="form-control" placeholder="Mot de passe" {{ Request::old("name") }} name="password" type="password" value="">
+                </div>
+                <div class="checkbox">
+                  <label>
+                    <input name="remember" type="checkbox" value="Remember Me"> Se souvenir de moi
+                   <input type="hidden" name="_token" value="{{ Session::token() }}">
+
+                  </label>
+                </div>
+              <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+            </fieldset>
+          </form>
+                      <hr/>
+                    <center><h4>Ou</h4></center>
+                    <input class="btn btn-primary btn-lg btn-facebook btn-block" type="submit" value="Connexion via facebook">
+          </div>
+      </div>
         </div>
     </div>
 </div>
-@endsection
+
+@stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@include('layout/partials/_nav')
 @section('content')
 <div class="container">
     <div class="row">
