63330ad0b6bacb4db9094838e45003fd6edff445
webwatson92 <webwatson92@gmail.com>
2017-09-28 18:01:31 +0200

    Application de cartographie

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:4ObN62QsAXieLSYNXDYz1DaYw6VK1TP/miX0xYjbfEM='),
 
     'cipher' => 'AES-256-CBC',
 
--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-235-88-58.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd9i452bnufgmpb'),
+            'username' => env('DB_USERNAME', 'rxfwzpmejojiwr'),
+            'password' => env('DB_PASSWORD', '862d6bbcdc3b069dc8209baec3fe80c0bcd08efc767bc6772507a34d64cbaead'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
